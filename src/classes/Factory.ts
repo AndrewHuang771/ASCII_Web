f3f8d15e62e17a9be14f9f12792b5c9b3c3aa9f4
AndrewHuang771 <ahuang771@gmail.com>
2025-06-14 13:53:41 -0400

    Make factories

---
+++ src/classes/Factory.ts
@@ -0,0 +1,38 @@
+export class RectangleFactory {
+    build(xMin: number, xMax: number, yMin: number, yMax: number, fill: Function, background: Function) {
+        return (x: number, y: number, t: number) => {
+            if ((x === xMin || x === xMax) && y > yMin && y < yMax) {
+                return "-";
+            } else if ((y === yMin || y === yMax) && x > xMin && x < xMax) {
+                return "|"
+            } else if ((x === xMin && y === yMin) || (x === xMax && y === yMax)) {
+                return "/"
+            } else if ((x === xMin && y === yMax) || (x === xMax && y === yMin)) {
+                return "\\"
+            } else if (x > xMin && x < xMax && y > yMin && y < yMax) {
+                return fill(x, y, t);
+            } else {
+                return background(x, y, t);
+            }
+        }
+    }
+}
+
+export class StaticFactory {
+    build(xMin: number, xMax: number, yMin: number, yMax: number, text: String[], background: Function) {
+        return (x: number, y: number, t: number) => {
+            if (x > xMin && x < xMax && y > yMin && y < yMax) {
+                x -= xMin + 1;
+                y -= yMin + 1;
+                if (x >= 0 && x < text.length) {
+                    if (y >= 0 && y < text[x].length) {
+                        return text[x][y];
+                    }
+                }
+                return " ";
+            } else {
+                return background(x, y, t);
+            }
+        }
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
-import { TextPlayer } from "./classes/TextPlayer";
+import { NoiseFunction3D, createNoise3D } from "simplex-noise";
+import { RectangleFactory, StaticFactory } from "./classes/Factory";
+
 import { Charmap } from "./classes/Charmap";
+import { TextPlayer } from "./classes/TextPlayer";
 import { scalemap } from "./config/ASCII";
 import { text } from "./assets/test_long";
-import { createNoise3D, NoiseFunction3D } from "simplex-noise";
 
 // Create 2 configs for a framemap based on simplex noise
 const noise3D: NoiseFunction3D = createNoise3D();
@@ -10,49 +12,14 @@ const generatorFunction = (x: number, y: number, t: number) => {
     const scalemapIdx = Math.floor(scalemap.length / 2) + Math.floor(scalemap.length / 2 * noise3D((x + 1) * 0.005, (y + 1) * 0.005, 0.005 * t));
     return scalemap[scalemapIdx];
 }
-const generatorFunction2 = (x: number, y: number, t: number) => {
-    const scalemapIdx = Math.floor(scalemap.length / 2) + Math.floor(scalemap.length / 2 * noise3D((x + 100000) * 0.005, (y + 100000) * 0.005, 0.005 * t));
-    return scalemap[scalemapIdx];
-}
 
-const generatorFunction4 = (x: number, y: number, t: number) => {
-    // if (t >= 1 && t <= 109) {
-    //     x -= Math.floor(t / 10);
-    //     y -= Math.floor(t / 10);
-    // } else {
-    x -= 11;
-    y -= 11;
-    // }
+const rectFactory = new RectangleFactory();
+const staticFactory = new StaticFactory();
 
-    if (x >= 0 && x < text.length) {
-        if (y >= 0 && y < text[x].length) {
-            return text[x][y];
-        }
-    }
-    return " ";
-}
+const staticFunction = staticFactory.build(10, 40, 10, 100, text, generatorFunction);
 
-const rectangleFunctionFactory = (xMin: number, xMax: number, yMin: number, yMax: number, fill: Function, background: Function) => {
-    const rectangleFunction = (x: number, y: number, t: number) => {
-        if ((x === xMin || x === xMax) && y > yMin && y < yMax) {
-            return "-";
-        } else if ((y === yMin || y === yMax) && x > xMin && x < xMax) {
-            return "|"
-        } else if ((x === xMin && y === yMin) || (x === xMax && y === yMax)) {
-            return "/"
-        } else if ((x === xMin && y === yMax) || (x === xMax && y === yMin)) {
-            return "\\"
-        } else if (x > xMin && x < xMax && y > yMin && y < yMax) {
-            return fill(x, y, t);
-        } else {
-            return background(x, y, t);
-        }
-    }
-    return rectangleFunction;
-}
-
-const sampleRectGenFun = rectangleFunctionFactory(10, 40, 10, 100, generatorFunction4, generatorFunction);
-const nestedRectFun = rectangleFunctionFactory(15, 25, 15, 30, generatorFunction, sampleRectGenFun)
+const sampleRectGenFun = rectFactory.build(10, 40, 10, 100, staticFunction, generatorFunction);
+const nestedRectFun = rectFactory.build(15, 25, 15, 30, generatorFunction, sampleRectGenFun)
 
 let testCharmap: Charmap = new Charmap(0, generatorFunction);
 let testCharmap2: Charmap = new Charmap(0, nestedRectFun);
